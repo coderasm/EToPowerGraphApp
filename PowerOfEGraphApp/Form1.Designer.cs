6f27a604df25823e933dc20d5c21e5f08dd38edf
pkrueger <philip.krueger@caci.com>
2019-05-09 20:42:34 -0700

    changable colors and interval

--- PowerOfEGraphApp/Form1.Designer.cs
+++ PowerOfEGraphApp/Form1.Designer.cs
@@ -33,7 +33,17 @@
       System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
       System.Windows.Forms.DataVisualization.Charting.Title title1 = new System.Windows.Forms.DataVisualization.Charting.Title();
       this.chart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+      this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+      this.colorsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+      this.intervalsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+      this.greenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+      this.blueToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+      this.redToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+      this.to5ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+      this.to47ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+      this.to20ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
       ((System.ComponentModel.ISupportInitialize)(this.chart)).BeginInit();
+      this.menuStrip1.SuspendLayout();
       this.SuspendLayout();
       // 
       // chart
@@ -44,19 +54,92 @@
       this.chart.ChartAreas.Add(chartArea1);
       legend1.Name = "Legend1";
       this.chart.Legends.Add(legend1);
-      this.chart.Location = new System.Drawing.Point(12, 12);
+      this.chart.Location = new System.Drawing.Point(12, 27);
       this.chart.Name = "chart";
       series1.ChartArea = "ChartArea1";
       series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
       series1.Legend = "Legend1";
       series1.Name = "e^x";
       this.chart.Series.Add(series1);
-      this.chart.Size = new System.Drawing.Size(761, 426);
+      this.chart.Size = new System.Drawing.Size(776, 411);
       this.chart.TabIndex = 0;
       this.chart.Text = "chart1";
       title1.Name = "Title1";
       title1.Text = "Graph of e^x";
       this.chart.Titles.Add(title1);
+      // 
+      // menuStrip1
+      // 
+      this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.colorsToolStripMenuItem,
+            this.intervalsToolStripMenuItem});
+      this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+      this.menuStrip1.Name = "menuStrip1";
+      this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+      this.menuStrip1.TabIndex = 1;
+      this.menuStrip1.Text = "menuStrip1";
+      // 
+      // colorsToolStripMenuItem
+      // 
+      this.colorsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.greenToolStripMenuItem,
+            this.blueToolStripMenuItem,
+            this.redToolStripMenuItem});
+      this.colorsToolStripMenuItem.Name = "colorsToolStripMenuItem";
+      this.colorsToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
+      this.colorsToolStripMenuItem.Text = "Colors";
+      // 
+      // intervalsToolStripMenuItem
+      // 
+      this.intervalsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.to5ToolStripMenuItem,
+            this.to47ToolStripMenuItem,
+            this.to20ToolStripMenuItem});
+      this.intervalsToolStripMenuItem.Name = "intervalsToolStripMenuItem";
+      this.intervalsToolStripMenuItem.Size = new System.Drawing.Size(63, 20);
+      this.intervalsToolStripMenuItem.Text = "Intervals";
+      // 
+      // greenToolStripMenuItem
+      // 
+      this.greenToolStripMenuItem.Name = "greenToolStripMenuItem";
+      this.greenToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+      this.greenToolStripMenuItem.Text = "Green";
+      this.greenToolStripMenuItem.Click += new System.EventHandler(this.onGreenClick);
+      // 
+      // blueToolStripMenuItem
+      // 
+      this.blueToolStripMenuItem.Name = "blueToolStripMenuItem";
+      this.blueToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+      this.blueToolStripMenuItem.Text = "Blue";
+      this.blueToolStripMenuItem.Click += new System.EventHandler(this.onBlueClick);
+      // 
+      // redToolStripMenuItem
+      // 
+      this.redToolStripMenuItem.Name = "redToolStripMenuItem";
+      this.redToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+      this.redToolStripMenuItem.Text = "Red";
+      this.redToolStripMenuItem.Click += new System.EventHandler(this.onRedClick);
+      // 
+      // to5ToolStripMenuItem
+      // 
+      this.to5ToolStripMenuItem.Name = "to5ToolStripMenuItem";
+      this.to5ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+      this.to5ToolStripMenuItem.Text = "4 to 5";
+      this.to5ToolStripMenuItem.Click += new System.EventHandler(this.on4to5Click);
+      // 
+      // to47ToolStripMenuItem
+      // 
+      this.to47ToolStripMenuItem.Name = "to47ToolStripMenuItem";
+      this.to47ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+      this.to47ToolStripMenuItem.Text = "2.3 to 4.7";
+      this.to47ToolStripMenuItem.Click += new System.EventHandler(this.on2pt3to4pt7Click);
+      // 
+      // to20ToolStripMenuItem
+      // 
+      this.to20ToolStripMenuItem.Name = "to20ToolStripMenuItem";
+      this.to20ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+      this.to20ToolStripMenuItem.Text = "0 to 20";
+      this.to20ToolStripMenuItem.Click += new System.EventHandler(this.on0to20Click);
       // 
       // Form1
       // 
@@ -64,17 +147,31 @@
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
       this.ClientSize = new System.Drawing.Size(800, 450);
       this.Controls.Add(this.chart);
+      this.Controls.Add(this.menuStrip1);
+      this.MainMenuStrip = this.menuStrip1;
       this.Name = "Form1";
       this.Text = "Graph of e^x";
       this.Load += new System.EventHandler(this.onLoad);
       ((System.ComponentModel.ISupportInitialize)(this.chart)).EndInit();
+      this.menuStrip1.ResumeLayout(false);
+      this.menuStrip1.PerformLayout();
       this.ResumeLayout(false);
+      this.PerformLayout();
 
     }
 
     #endregion
 
     private System.Windows.Forms.DataVisualization.Charting.Chart chart;
+    private System.Windows.Forms.MenuStrip menuStrip1;
+    private System.Windows.Forms.ToolStripMenuItem colorsToolStripMenuItem;
+    private System.Windows.Forms.ToolStripMenuItem greenToolStripMenuItem;
+    private System.Windows.Forms.ToolStripMenuItem blueToolStripMenuItem;
+    private System.Windows.Forms.ToolStripMenuItem redToolStripMenuItem;
+    private System.Windows.Forms.ToolStripMenuItem intervalsToolStripMenuItem;
+    private System.Windows.Forms.ToolStripMenuItem to5ToolStripMenuItem;
+    private System.Windows.Forms.ToolStripMenuItem to47ToolStripMenuItem;
+    private System.Windows.Forms.ToolStripMenuItem to20ToolStripMenuItem;
   }
 }
 
--- PowerOfEGraphApp/Form1.cs
+++ PowerOfEGraphApp/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Drawing;
 using System.Windows.Forms;
 
 namespace PowerOfEGraphApp
@@ -13,15 +14,55 @@ namespace PowerOfEGraphApp
 
     private void onLoad(object sender, EventArgs e)
     {
-      fillChart();
+      fillChart(4, 5, .1);
     }
 
-    private void fillChart()
+    private void fillChart(double min, double max, double step)
     {
-      for (double i = 4; i <= 5; i+=.1)
+      chart.Series[0].Points.Clear();
+      for (double i = min; i <= max; i+=step)
       {
         chart.Series[0].Points.AddXY(i, Math.Pow(Math.E, i));
       }
+    }
+
+    private void on4to5Click(object sender, EventArgs e)
+    {
+      chart.ChartAreas[0].AxisX.Minimum = 4;
+      chart.ChartAreas[0].AxisX.Maximum = 5;
+      chart.ChartAreas[0].AxisX.Interval = .1;
+      fillChart(4, 5, .1);
+    }
+
+    private void on2pt3to4pt7Click(object sender, EventArgs e)
+    {
+      chart.ChartAreas[0].AxisX.Minimum = 2.3;
+      chart.ChartAreas[0].AxisX.Maximum = 4.7;
+      chart.ChartAreas[0].AxisX.Interval = .1;
+      fillChart(2.3, 4.7, .1);
+    }
+
+    private void on0to20Click(object sender, EventArgs e)
+    {
+      chart.ChartAreas[0].AxisX.Minimum = 0;
+      chart.ChartAreas[0].AxisX.Maximum = 20;
+      chart.ChartAreas[0].AxisX.Interval = .5;
+      fillChart(0, 20, .5);
+    }
+
+    private void onGreenClick(object sender, EventArgs e)
+    {
+      chart.Series[0].Color = Color.Green;
+    }
+
+    private void onBlueClick(object sender, EventArgs e)
+    {
+      chart.Series[0].Color = Color.Blue;
+    }
+
+    private void onRedClick(object sender, EventArgs e)
+    {
+      chart.Series[0].Color = Color.Red;
     }
   }
 }
